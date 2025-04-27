7148f36e3badeeeb48db1dabf0984312e3290e7d
Nicky2023SPX <solonikko23@gmail.com>
2025-04-27 20:09:07 +0200

    Update Simple2DCameraFollow.cs

--- examples/Simple2DCameraFollow.cs
+++ examples/Simple2DCameraFollow.cs
@@ -1,1 +1,21 @@
-// Simple movement script - (c) Niccolò Sorrentino
+// Simple 2d cameramovement script - (c) Niccolò Sorrentino
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform cart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, cart.position + new Vector3(0, 0, -10), Time.deltaTime * 10);
+    }
+}
