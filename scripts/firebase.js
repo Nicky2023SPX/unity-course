d477acd6e0fe8f6341ad12667bcb16b19d4eb5d0
Nicky2023SPX <solonikko23@gmail.com>
2025-11-30 18:47:45 +0100

    Add Firebase authentication with Google sign-in

---
+++ scripts/firebase.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+// Import the functions you need from the SDKs you need
+window.onload = function() {
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "${{ secrets.google_api_key }}",
+    authDomain: "corso-di-unity.firebaseapp.com",
+    projectId: "corso-di-unity",
+    storageBucket: "corso-di-unity.firebasestorage.app",
+    messagingSenderId: "387995337831",
+    appId: "1:387995337831:web:f1a285590731e8664800c2"
+  };
+
+  const loginButton = document.querySelector('.login-btn');
+  const loginImageDiv = document.querySelector('.login-btn-image');
+  const loginUsername = document.querySelector('.login-btn-username');
+
+  if (localStorage.getItem("email") != null) {
+    loginImageDiv.innerHTML = `<img src="${localStorage.getItem('photo_url')}">`;
+    loginUsername.innerHTML = `${localStorage.getItem('username')}`;
+    loginButton.classList.add('w3-disabled');
+  }
+  else
+  {
+    loginButton.addEventListener('click', function() {
+        signInWithGooglePopup();
+    });
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  function signInWithGooglePopup() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // L'accesso è riuscito. L'oggetto 'result' contiene informazioni sull'utente.
+      const user = result.user;
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+      
+      console.log("Accesso con Google riuscito:", user.photoURL);
+
+      localStorage.setItem('username', user.displayName);
+      localStorage.setItem('email', user.email);
+      localStorage.setItem('photo_url', user.photoURL);
+
+      window.location.href = "";
+      
+      // Reindirizza l'utente o aggiorna l'UI.
+    })
+    .catch((error) => {
+      // Gestione degli errori.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+
+      console.error("Errore nell'accesso con Google:", errorMessage);
+    });
+  }
+}
