390c8958c0b67d4ec1afd2a089d592b3353f11a7
Nicky2023SPX <solonikko23@gmail.com>
2025-12-02 20:21:49 +0100

    Add files via upload

--- scripts/space.js
+++ scripts/space.js
@@ -3,6 +3,11 @@ const email = document.querySelector('.emailSpace');
 const image = document.querySelector('.imageSpace');
 const completedLessons = document.querySelector('.emailSpace');
 
+if (localStorage.getItem('email') == null)
+{
+    document.querySelector('#profile').classList.add("blurred");
+}
+
 
 username.innerHTML = localStorage.getItem('username');
 email.innerHTML = localStorage.getItem('email');
